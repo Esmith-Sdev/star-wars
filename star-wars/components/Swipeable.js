127726f71f2714c06712a89a009293a24531beda
Esmith-Sdev <esmith897@ivytech.edu>
2026-04-18 11:06:19 -0400

    adding reanimated, replacing current swipe function with Gesture

--- star-wars/components/Swipeable.js
+++ star-wars/components/Swipeable.js
@@ -1,7 +1,16 @@
 import React from "react";
 import { View, ScrollView, Text, TouchableOpacity } from "react-native";
 import styles from "../styles";
+import Animated, {
+  SlideInLeft,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+  withTiming,
+} from "react-native-reanimated";
+import { GestureDetector, Gesture } from "react-native-gesture-handler";
 export default function Swipeable({ onSwipe, name }) {
+  const pressed = useSharedValue(false);
   function onScroll(e) {
     e.nativeEvent.contentOffset.x >= 200 && onSwipe();
   }
@@ -9,18 +18,45 @@ export default function Swipeable({ onSwipe, name }) {
     horizontal: true,
     pagingEnabled: true,
     showsHorizontalScrollIndicator: false,
+    showsVerticalScrollIndicator: false,
     scrollEventThrottle: 10,
     onScroll,
   };
+  const offset = useSharedValue(0);
+  const animatedStyles = useAnimatedStyle(() => ({
+    transform: [
+      { translateX: offset.value },
+      { scale: withTiming(pressed.value ? 1.2 : 1) },
+    ],
+    backgroundColor: pressed.value ? "#e5703d" : "#fff",
+  }));
+
+  const pan = Gesture.Pan()
+    .onBegin(() => {
+      pressed.value = true;
+    })
+
+    .onChange((event) => {
+      offset.value = event.translationX;
+    })
+
+    .onFinalize(() => {
+      if (offset.value <= 200) {
+        onSwipe();
+      }
+      offset.value = withSpring(0);
+      pressed.value = false;
+    });
   return (
     <View style={styles.swipeContainer}>
       <ScrollView {...scrollProps}>
-        <TouchableOpacity>
-          <View style={styles.swipeItem}>
-            <Text style={styles.swipeItemText}>{name}</Text>
-          </View>
-        </TouchableOpacity>
-        <View style={styles.swipeBlank} />
+        <Animated.View entering={SlideInLeft}>
+          <GestureDetector gesture={pan}>
+            <Animated.View style={[styles.swipeItem, animatedStyles]}>
+              <Text style={styles.swipeItemText}>{name}</Text>
+            </Animated.View>
+          </GestureDetector>
+        </Animated.View>
       </ScrollView>
     </View>
   );
